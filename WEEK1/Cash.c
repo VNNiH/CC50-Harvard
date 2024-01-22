4156977dae753779db4399b70937335a5566d3c4
Vinicius Benassuli <107008220+VNNiH@users.noreply.github.com>
2024-01-22 16:28:33 -0300

    Create Cash.c

---
+++ WEEK1/Cash.c
@@ -0,0 +1,43 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    float dollars;
+    do
+    {
+        dollars = get_float("dollars: ");
+    }
+    while (dollars < round(0.01));
+
+    int cents = round(dollars * 100);
+    int coins = 0;
+
+
+    while (cents > 0)
+    {
+        if (cents >= 25)
+        {
+            cents -= 25;
+            coins++;
+        }
+        else if (cents >= 10)
+        {
+            cents -= 10;
+            coins++;
+        }
+        else if (cents >= 5)
+        {
+            cents -= 5;
+            coins++;
+        }
+        else
+        {
+            cents -= 1;
+            coins++;
+        }
+    }
+
+    printf("%d \n", coins);
+}
